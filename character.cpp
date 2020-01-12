1eccea122a4d739f0fbedc3ae8be0f3d29a30cde
Langorithm <sebastian.lang.maso@gmail.com>
2020-01-12 16:01:08 -0300

    Began implementing character class

---
+++ character.cpp
@@ -0,0 +1,88 @@
+#include "character.h"
+
+string Character::getName(){
+	return _name;
+}
+
+void Character::setName(string s){
+	_name = s;
+}
+
+map<Job, int> Character::getLevels(){
+	return _jobs;
+}
+
+int Character::getCharacterLevel(){
+	int res = 0;
+	
+	for (Job j : _jobs){
+		res += _jobs[j];
+	}
+
+}
+
+int Character::getProfBonus(){
+	int res = getCharacterLevel;
+	res /= 4;
+	res += 2;
+	return res;
+}
+
+int Character::getMaxHP(){
+	return _maxHP;
+}
+
+int Character::getInitiative(){
+	return _Initiative;
+}
+
+int Character::getSpeed(){
+	return _speed;
+}
+
+int Character::getPassivePerception(){
+	return 10 + getRollMod("Perception");
+}
+
+
+void Character::setMaxHP(int n){
+	_maxHP = n;
+}
+
+void Character::setInitiative(int n){
+	_initiative = n;
+}
+
+void Character::setSpeed(int n){
+	_speed = n;
+}
+
+abilityScores Character::getScores(){
+	return _scores;
+
+}
+
+int Character::getAbilityMod(Ability a){
+	return abilityMod(
+		getScores()[a]
+	);
+}
+
+int Character::getRollMod(proficiency P){
+	//TO DO
+	return 0;
+}
+
+
+set<proficiency> Character::getProficiencies(){
+	return _proficiencies;
+}
+
+set<Feature> Character::getFeatures(){
+	return _features;
+}
+
+
+
+
+
---
+++ character.h
@@ -0,0 +1,46 @@
+#include "basics.h"
+
+class Character{
+
+public:
+	string 		getName();
+	void 		setName(string s);
+
+	map<Job, int>	getLevels();
+	string		getRaceName();
+
+	int		getCharacterLevel();
+	int		getProfBonus();
+	int 		getMaxHP();
+	int		getInitiative();
+	int		getSpeed();
+	int		getPassivePerception();
+	
+	void 		setMaxHP(int n);
+	void		setInitiative(int n);
+	void		setSpeed(int n);
+
+	abilityScores		getScores();
+	int			getAbilityMod(Ability a);
+	int 			getRollMod(proficiency p);
+	set<proficiency>	getProficiencies();
+	set<Feature>		getFeatures();
+	
+	Character(Race r, Job firstLevel, string bg);
+
+	void level up(Job j);
+	
+private:
+	string 			_name;
+	Race&			_race;
+	map<Job, int> 		_jobs;
+	string			_background;
+	abilityScores		_scores;
+	set<proficiency>	_proficiencies;		
+	set<Feature>		_features;
+
+	int			_maxHP;	
+	int			_initiative;
+	int			_speed;
+	
+}
